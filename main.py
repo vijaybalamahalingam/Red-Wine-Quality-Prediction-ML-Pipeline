7faf60d4829167dface8e6245a7680cce099e69e
Vijay <vijay_5@Vijays-MacBook-Air.local>
2025-01-17 14:29:25 +0530

    feat: add data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from redwine import logger
 from redwine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from redwine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from redwine.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 
@@ -19,6 +20,17 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
