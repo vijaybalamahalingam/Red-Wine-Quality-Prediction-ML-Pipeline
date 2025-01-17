ca89350d7e4783928c29073cc5a145ee006c107c
Vijay <vijay_5@Vijays-MacBook-Air.local>
2025-01-17 14:11:56 +0530

    feat: add data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from redwine import logger
 from redwine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from redwine.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -13,3 +14,13 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/redwine/pipeline/stage_02_data_validation.py
@@ -0,0 +1,27 @@
+from redwine.config.configuration import ConfigurationManager
+from redwine.components.data_validation import DataValiadtion
+from redwine import logger
+
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======")
+    except Exception as e:
+        logger.exception(e)
+        raise e    
